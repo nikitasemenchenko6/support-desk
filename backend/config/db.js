ed8f41e3c2b85793d4dd912f716fdd59148033af
gu-log <113324088+gu-log@users.noreply.github.com>
2023-02-11 03:58:26 +0900

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,8 +5,8 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI)
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
   } catch (error) {
-    console.log(`Error: ${error.message}`.red.underline.bold)
-    process.exit(1)
+    console.log(`Error: ${error.message}`.red.underline.bold);
+    process.exit(1);
   }
 }
 
